ca9b51e16aff5f70973eb06759ad76866598dd27
Sannim <inegg.apps@gmail.com>
2020-09-02 18:16:21 +0900

    최상위 컴포넌트인 App.vue 설정하고 main.js 에서 App.vue와 연결하기

--- vue-03-todo/main.js
+++ vue-03-todo/main.js
@@ -1,2 +1,15 @@
-//진입점으로 사용되는 파일
-console.log('Hello webpack!');
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  el: '#app',
+  //   render(createElement) {
+  //     return createElement(App);
+  //   },
+  render: (h) => h(App),
+});
+
+// main.js: 진입점으로 사용되는 파일로서 App.vue를 연결할 것임.
+// App.js는 모든 앱을 아우르는 최상위 컴포넌트로써 사용될 것임.
+
+// console.log('Hello webpack!');
