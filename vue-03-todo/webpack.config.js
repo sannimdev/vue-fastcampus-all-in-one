f005962e30bd975820211743e53220b092957019
Sannim <inegg.apps@gmail.com>
2020-09-02 17:45:28 +0900

    뷰 로더 설치하고 webpack 세팅하기

--- vue-03-todo/webpack.config.js
+++ vue-03-todo/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path'); //nodejs에서 path모듈 자체를 제공하므로 경로를 명시할 필요는 없음
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
   //진입점 (entry)
   //프로젝트가 돌아가기 위해 가장 먼저 실행되어야 하는 파일
@@ -11,8 +13,17 @@ module.exports = {
     filename: '[name].js', //app.js ([name]은 진입점의 app이라는 키 이르믕ㄹ 참조)
     path: path.join(__dirname, 'dist'),
   },
-  module: {},
+  module: {
+    ///// https://vue-loader.vuejs.org/guide/#manual-setup 에서 복붙
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
+    /////
+  },
   // plugins만 복수형
-  plugins: [],
+  plugins: [new VueLoaderPlugin()],
 };
 //웹팩은 nodejs 런타임에서 실행
