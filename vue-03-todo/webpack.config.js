7f088ccad94027a77601b009647f770af02387b6
Sannim <inegg.apps@gmail.com>
2020-09-02 18:26:23 +0900

    webpack 사용에 필요한 로더를 설치하고 세팅 진행하기 (but @babel/core 미설치)

--- vue-03-todo/webpack.config.js
+++ vue-03-todo/webpack.config.js
@@ -20,6 +20,17 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
       },
+      {
+        test: /\.js$/,
+        //node_modules 안에 있는 모듈은 굳이 바벨로 해석할 필요가 없음
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        //최신버전에서는 loader가 아니라 use를 명시하여도 동작한다. (use: 'babel-loader')
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
     ],
     /////
   },
@@ -27,3 +38,23 @@ module.exports = {
   plugins: [new VueLoaderPlugin()],
 };
 //웹팩은 nodejs 런타임에서 실행
+
+/* 
+주의사항
+
+ERROR in ./App.vue?vue&type=style&index=0&id=472cff63&scoped=true&lang=css& (./node_modules/vue-loader/lib??vue-loader-options!./App.vue?vue&type=style&index=0&id=472cff63&scoped=true&lang=css&) 16:3
+Module parse failed: Unexpected token (16:3)
+File was processed with these loaders:
+ * ./node_modules/vue-loader/lib/index.js
+You may need an additional loader to handle the result of these loaders.
+|
+|
+> h1 {
+|   color: red;
+| }
+ @ ./App.vue?vue&type=style&index=0&id=472cff63&scoped=true&lang=css& 1:0-146 1:162-165 1:167-310 1:167-310
+ @ ./App.vue
+ @ ./main.js
+
+ 스크립트, 스타일까지 해석할 수 있는 로더를 설정하지 않으면 번들링 시 위와 같은 오류가 발생한다.
+*/
