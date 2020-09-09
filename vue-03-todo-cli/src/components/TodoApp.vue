1cac4c7a187fb5a21e552f417db0de5e7984c664
Sannim <inegg.apps@gmail.com>
2020-09-09 22:03:14 +0900

    db에 데이터를 삽입하기 위한 구조 작성. id는 crypto-random-string 모듈을 이용한다.

--- vue-03-todo-cli/src/components/TodoApp.vue
+++ vue-03-todo-cli/src/components/TodoApp.vue
@@ -6,12 +6,51 @@
 </template>
 
 <script>
+import lowdb from "lowdb";
+import LocalStorage from "lowdb/adapters/LocalStorage";
+import cryptoRandomString from "crypto-random-string";
+
 import TodoCreator from "./TodoCreator";
 import TodoItem from "./TodoItem";
+
 export default {
   components: {
     TodoCreator,
     TodoItem,
+  },
+  data() {
+    return {
+      db: null,
+    };
+  },
+  created() {
+    this.initDB();
+  },
+  methods: {
+    initDB() {
+      const adapter = new LocalStorage("todo-app"); //DB 이름
+      //db와 어댑터 연결
+      this.db = lowdb(adapter); //?
+      //Local DB 초기화
+      this.db
+        .defaults({
+          todos: [], //Collection
+        })
+        .write();
+    },
+    createTodo(title) {
+      const newTodo = {
+        id: cryptoRandomString({ length: 10 }),
+        title,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        done: false,
+      };
+
+      //get과 push는 lodash에서 제공하는 메서드
+      //write는 lowdb에서 제공하는 메서드로 실제 db에 변경사항을 반영하고자 한다면 꼭 호출해야
+      this.db.get("todos").push(newTodo).write();
+    },
   },
 };
 </script>
