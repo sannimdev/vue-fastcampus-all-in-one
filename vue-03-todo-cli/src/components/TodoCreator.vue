4e0fb7febbf37acb9d0958f71eae7e3a3ae1749c
Sannim <inegg.apps@gmail.com>
2020-09-10 20:13:06 +0900

    할일 추가하기 (LocalStorage에 반영됨)

--- vue-03-todo-cli/src/components/TodoCreator.vue
+++ vue-03-todo-cli/src/components/TodoCreator.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Todo creator</h1>
+  <div>
+    <button @click="createTodo">추가</button>
+    <input
+      type="text"
+      :value="title"
+      :placeholder="placeholder"
+      @input="title = $event.target.value"
+      @keypress.enter="createTodo"
+    />
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      placeholder: "할일을 추가하세요🙂",
+    };
+  },
+  methods: {
+    createTodo() {
+      //생성
+      const validatedTitle = this.title.trim();
+      if (!validatedTitle) {
+        alert("유효하지 않은 제목입니다.");
+        this.title = this.title.trim();
+        return;
+      }
+      this.$emit("create-todo", this.title);
+      this.title = "";
+    },
+  },
+};
+</script>
