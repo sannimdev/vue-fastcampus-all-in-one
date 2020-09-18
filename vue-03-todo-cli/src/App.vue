1549fd0456f6ef06a7cad01460eb39831b748632
Sannim <inegg.apps@gmail.com>
2020-09-18 21:31:45 +0900

    router-link 태그를 이용하여 홈 버튼 만들기
    * 현재 페이지인지 아닌지 구분하는 방법까지

--- vue-03-todo-cli/src/App.vue
+++ vue-03-todo-cli/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <RouterView />
+  <div>
+    <RouterLink to="/" class="to-home">
+      <i class="material-icons">home</i>
+    </RouterLink>
+    <RouterView />
+  </div>
 </template>
 
 <style>
-@import url('https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css');
-@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap');
+@import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap");
+
+.router-link-exact-active {
+  display: none;
+}
 </style>
