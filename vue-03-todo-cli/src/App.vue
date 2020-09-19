9a0d4b401bfa739c89dac49bfb7a0f637e669c60
Sannim <inegg.apps@gmail.com>
2020-09-19 12:39:04 +0900

    - Router와 Route객체에 대해 알아보기 - path를 지정하여 라우팅하는 것뿐 아니라 name속성으로 각 라우트에 관한 이름을 지정할 수 있으며, children으로 서브 라우팅도 할 수 있음.

--- vue-03-todo-cli/src/App.vue
+++ vue-03-todo-cli/src/App.vue
@@ -7,6 +7,17 @@
   </div>
 </template>
 
+<script>
+export default {
+  created() {
+    //라우터는 뷰 라우터에서 제공하는 정보를 나타냄 (세팅해 놓은 결괏값)
+    console.log("router", this.$router);
+    //라우트는
+    console.log("route", this.$route);
+  },
+};
+</script>
+
 <style>
 @import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
 @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
--- vue-03-todo-cli/src/router/index.js
+++ vue-03-todo-cli/src/router/index.js
@@ -10,16 +10,26 @@ Vue.use(VueRouter);
 const routes = [
   //config
   {
+    name: 'index',
     path: '/',
     component: Home,
   },
   {
+    name: 'about',
     path: '/about',
     component: About,
   },
   {
+    name: 'todos',
     path: '/todos',
     component: TodoApp,
+    children: [
+      //또 하나의 라우트 객체라고 보면 된다.
+      {
+        name: 'todos-filter',
+        path: ':id', //받을 param의 이름을 적는다.
+      },
+    ],
   },
 ];
 
