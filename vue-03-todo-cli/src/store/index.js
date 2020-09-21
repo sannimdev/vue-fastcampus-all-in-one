0cdcae62a2448b0251ff6b4de9d3f5e22c3f8d62
Sannim <inegg.apps@gmail.com>
2020-09-21 22:05:00 +0900

    Vuex 오리엔테이션

--- vue-03-todo-cli/src/store/index.js
+++ vue-03-todo-cli/src/store/index.js
@@ -1,9 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import todoApp from './todoApp';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  //   strict: true, //배포 시에는 false, 개발 시에는 true
+  //strict: true, //배포 시에는 false, 개발 시에는 true
   strict: process.env.NODE_ENV !== 'production',
+  modules: {
+    todoApp,
+  },
+  // //Data
+  // state: {},
+  // //Methods
+  // //실제 값을 변경할 때 사용 (비동기 처리를 할 수 없음)
+  // //state를 변경할 수 있는 권한이 mutations 이하에 주어진다.
+  // mutations: {},
+  // //Methods
+  // //비동기를 포함한 일반 로직을 작성(비동기 O)
+  // //actions에서는 state값을 직접 변경할 수가 없음.
+  // actions: {},
 });
---
+++ vue-03-todo-cli/src/store/todoApp.js
@@ -0,0 +1,7 @@
+export default {
+  namespaced: true, //독립적으로 관리하기 위함
+  state: () => ({}), //arrow function 문법으로 작성 //state도 data와 마찬가지로 함수여야 한다.
+  getters: {},
+  mutations: {},
+  actions: {},
+};
