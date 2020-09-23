120ffc288bb9fec6976277e4e5adc0aaa581b92d
Sannim <inegg.apps@gmail.com>
2020-09-23 20:59:18 +0900

    TodoApp.vue => todoApp.js(Vuex 상태관리 저장소)로 이관하기 작업까지 완료
     (단, 아직 프로그램이 오류 없이 실행되나 정상적으로 동작하지 않는 시점의 커밋이라는 것에 유의)

--- vue-03-todo-cli/src/store/todoApp.js
+++ vue-03-todo-cli/src/store/todoApp.js
@@ -1,7 +1,148 @@
+import Vue from 'vue';
+import cryptoRandomString from 'crypto-random-string';
+import lowdb from 'lowdb';
+import LocalStorage from 'lowdb/adapters/LocalStorage';
+import _find from 'lodash/find';
+import _assign from 'lodash/assign';
+import _cloneDeep from 'lodash/cloneDeep';
+import _findIndex from 'lodash/findIndex';
+import _forEachRight from 'lodash/forEachRight';
+
 export default {
   namespaced: true, //독립적으로 관리하기 위함
-  state: () => ({}), //arrow function 문법으로 작성 //state도 data와 마찬가지로 함수여야 한다.
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: () => ({
+    db: null,
+    todos: [],
+  }), //arrow function 문법으로 작성 //state도 data와 마찬가지로 함수여야 한다.
+  getters: {
+    total(state) {
+      return state.todos.length;
+    },
+    activeCount(state) {
+      return state.todos.filter((todo) => !todo.done).length;
+    },
+    completedCount(state, getters) {
+      return getters.total - getters.activeCount;
+    },
+  },
+  mutations: {
+    //궁극적인 데이터 변경은 가능하나 비동기 로직 처리는 불가능함
+    assignDB(state, db) {
+      //state로 바로 접근할 수 있도록 첫 번째 인수로 받음
+      state.db = db;
+    },
+    createDB(state, newTodo) {
+      //get과 push는 lodash에서 제공하는 메서드
+      //write는 lowdb에서 제공하는 메서드로 실제 db에 변경사항을 반영하고자 한다면 꼭 호출해야
+      state.db
+        .get('todos')
+        .push(newTodo)
+        .write();
+    },
+    updateDB(state, { todo, value }) {
+      state.db
+        .get('todos')
+        .find({ id: todo.id })
+        .assign(value);
+    },
+    deleteDB(state, todo) {
+      state.db
+        .get('todos')
+        .remove({ id: todo.id })
+        .write();
+    },
+    assignTodos(state, todos) {
+      state.todos = todos;
+    },
+    pushTodo(state, newTodo) {
+      state.todos.push(newTodo);
+    },
+    assignTodo(state, { foundTodo, value }) {
+      _assign(foundTodo, value);
+    },
+    deleteTodo(state, foundIndex) {
+      Vue.delete(state.todos, foundIndex);
+    },
+    updateTodo(state, { todo, key, value }) {
+      todo[key] = value;
+    },
+  },
+  actions: {
+    //actions는 일반적인 logic을 처리할 수 있으나 데이터 변경이 불가능함 (=> mutations를 경유해야 함.)
+    initDB({ state, commit }) {
+      //첫 번째로 담고 있는 context 인수는  state, getters, mutations, actions를 담고 있음
+      const adapter = new LocalStorage('todo-app'); //DB 이름
+      //db와 어댑터 연결
+      commit('assignDB', lowdb(adapter));
+      const hasTodos = state.db.has('todos').value();
+
+      if (hasTodos) {
+        //DB의 모든 내용을 복사 (Deep)
+        commit('assignTodos', _cloneDeep(state.db.getState().todos));
+      } else {
+        //LocalDB 초기화
+        state.db
+          .defaults({
+            todos: [], //Collection
+          })
+          .write();
+      }
+    },
+    createTodo({ state, commit }, title) {
+      const newTodo = {
+        id: cryptoRandomString({ length: 10 }),
+        title,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        done: false,
+      };
+
+      //Create DB
+      commit('createDB', newTodo);
+
+      //Create Client
+      commit('pushTodo', newTodo);
+    },
+    updateTodo({ state }, { todo, value }) {
+      //Update DB
+      commit('updateDB', { todo, value });
+      const foundTodo = _find(state.todos, { id: todo.id });
+      commit('assignTodo', { foundTodo, value });
+    },
+    deleteTodo({ state }, todo) {
+      //DB에서 삭제
+      commit('deleteDB', todo);
+      //lodash의 remove는 반응성을 가지지 못한다.
+      // _remove(state.todos, { id: todo.id });
+
+      //Index값을 찾은 후 이를 바탕으로 원소 지우기
+      const foundIndex = _findIndex(state.todos, { id: todo.id });
+      //Vue를 이용한 delete메서드와 this.$delete메서드와는 같다.
+      //Delete Client
+      commit('deleteTodo', foundIndex);
+    },
+    completeAll({ state, commit }, checked) {
+      //DB commit에서는 반환값을 받아낼 수가 없는 구조이다.
+      // 따라서 newTodos를 통해서 새로운 값을 알아내지 못하므로 commit으로 이관하는 작업을 온전히 수행할 수가 없다.
+      const newTodos = state.db
+        .get('todos')
+        .forEach((todo) => {
+          //그래서 이쪽 부분만 mutation으로 이관해야 하는 작업을 수행해야 한다.
+          commit('updateTodo', {
+            todo,
+            key: 'done',
+            value: checked,
+          });
+        })
+        .write();
+      state.todos = _cloneDeep(newTodos);
+    },
+    clearCompleted({ state, dispatch }) {
+      _forEachRight(state.todos, (todo) => {
+        if (todo.done) {
+          dispatch.deleteTodo(todo);
+        }
+      });
+    },
+  },
 };
--- vue-03-todo-cli/src/views/TodoApp.vue
+++ vue-03-todo-cli/src/views/TodoApp.vue
@@ -42,15 +42,6 @@
 </template>
 
 <script>
-import lowdb from "lowdb";
-import LocalStorage from "lowdb/adapters/LocalStorage";
-import cryptoRandomString from "crypto-random-string";
-import _cloneDeep from "lodash/cloneDeep";
-import _find from "lodash/find";
-import _assign from "lodash/assign";
-// import _remove from "lodash/remove";
-import _findIndex from "lodash/findIndex";
-import _forEachRight from "lodash/forEachRight";
 import scrollTo from "scroll-to";
 import TodoCreator from "@/components/TodoCreator";
 import TodoItem from "@/components/TodoItem";
@@ -59,12 +50,6 @@ export default {
   components: {
     TodoCreator,
     TodoItem,
-  },
-  data() {
-    return {
-      db: null,
-      todos: [],
-    };
   },
   computed: {
     filteredTodos() {
@@ -79,15 +64,6 @@ export default {
           return this.todos.filter((todo) => todo.done);
       }
     },
-    total() {
-      return this.todos.length;
-    },
-    activeCount() {
-      return this.todos.filter((todo) => !todo.done).length;
-    },
-    completedCount() {
-      return this.total - this.activeCount;
-    },
     allDone: {
       get() {
         return this.total === this.completedCount && this.total > 0;
@@ -101,101 +77,6 @@ export default {
     this.initDB();
   },
   methods: {
-    initDB() {
-      const adapter = new LocalStorage("todo-app"); //DB 이름
-      //db와 어댑터 연결
-      this.db = lowdb(adapter); //?
-
-      const hasTodos = this.db.has("todos").value();
-
-      if (hasTodos) {
-        //DB의 모든 내용을 복사 (Deep)
-        this.todos = _cloneDeep(this.db.getState().todos);
-      } else {
-        //LocalDB 초기화
-        this.db
-          .defaults({
-            todos: [], //Collection
-          })
-          .write();
-      }
-    },
-    createTodo(title) {
-      const newTodo = {
-        id: cryptoRandomString({ length: 10 }),
-        title,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        done: false,
-      };
-
-      //get과 push는 lodash에서 제공하는 메서드
-      //write는 lowdb에서 제공하는 메서드로 실제 db에 변경사항을 반영하고자 한다면 꼭 호출해야
-      this.db.get("todos").push(newTodo).write();
-
-      //Create Client
-      this.todos.push(newTodo);
-    },
-    updateTodo(todo, value) {
-      this.db.get("todos").find({ id: todo.id }).assign(value).write();
-
-      const foundTodo = _find(this.todos, { id: todo.id });
-      _assign(foundTodo, value);
-    },
-    deleteTodo(todo) {
-      //DB에서 삭제
-      this.db.get("todos").remove({ id: todo.id }).write();
-
-      //lodash의 remove는 반응성을 가지지 못한다.
-      // _remove(this.todos, { id: todo.id });
-
-      //Index값을 찾은 후 이를 바탕으로 원소 지우기
-      const foundIndex = _findIndex(this.todos, { id: todo.id });
-      this.$delete(this.todos, foundIndex);
-    },
-    completeAll(checked) {
-      //DB
-      const newTodos = this.db
-        .get("todos")
-        .forEach((todo) => {
-          todo.done = checked;
-        })
-        .write();
-      //Local Todos
-      // this.todos.forEach((todo) => (todo.done = checked));
-      this.todos = _cloneDeep(newTodos);
-    },
-    clearCompleted() {
-      //DB에 반영
-      //배열을 삭제할 때 앞에서부터 삭제하면 당겨지므로 정상적으로 원하는 아이템을 삭제할 수 없을 수도 있다.
-      //따라서 맨 뒤에서부터 삭제한다면 원활하게 삭제를 할 수 있겠지? (오.....)
-      // this.todos.forEach((todo) => {
-      //   if (todo.done) {
-      //     this.deleteTodo(todo);
-      //   }
-      // });
-
-      //#1. Native (Reduce with reverse)
-      //TODO: Reduce 복습하기
-      // this.todos
-      //   .reduce((list, todo, index) => {
-      //     if (todo.done) {
-      //       list.push(index); //지워야 하는 index값만 보관하고 지우고자 함.
-      //     }
-      //     return list;
-      //   }, [])
-      //   .reverse()
-      //   .forEach((index) => {
-      //     this.deleteTodo(this.todos[index]);
-      //   });
-
-      //#2. lodash
-      _forEachRight(this.todos, (todo) => {
-        if (todo.done) {
-          this.deleteTodo(todo);
-        }
-      });
-    },
     scrollToTop() {
       scrollTo(0, 0, {
         ease: "linear",
