07dfadf9960208b1c05ac979f13faa8b093afa15
Sannim <inegg.apps@gmail.com>
2020-09-27 21:35:09 +0900

    Vuetify 컴포넌트를 사용하여 검색 컴포넌트 작성하기

--- vue-04-movie-app/src/components/MovieList.vue
+++ vue-04-movie-app/src/components/MovieList.vue
@@ -1,3 +1,6 @@
+<template>
+  <div>Movie</div>
+</template>
 
 <script>
 export default {};
--- vue-04-movie-app/src/components/SearchBar.vue
+++ vue-04-movie-app/src/components/SearchBar.vue
@@ -1,4 +1,38 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="title"
+      outlined
+      prepend-inner-icon="mdi-map-marker"
+      @keypress.enter="searchMovies"
+    >
+      <template v-slot:prepend-inner>
+        <v-icon>search</v-icon>
+      </template>
+      <template v-slot:append>
+        <v-progress-circular
+          v-if="loading"
+          size="24"
+          indeterminate
+          color="primary"
+        />
+      </template>
+    </v-text-field>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: '',
+      loading: false
+    };
+  },
+  methods: {
+    searchMovies() {
+      console.log('searchMovies');
+    }
+  }
+};
 </script>
