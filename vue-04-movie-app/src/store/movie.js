d7f140323a28e5bcfa9047c5c40f8a68404cd803
Sannim <inegg.apps@gmail.com>
2020-10-02 19:56:43 +0900

    영화 목록을 최대 40개까지 가져오기

--- vue-04-movie-app/src/store/movie.js
+++ vue-04-movie-app/src/store/movie.js
@@ -14,6 +14,9 @@ export default {
       Object.keys(payload).forEach(key => {
         state[key] = payload[key];
       });
+    },
+    pushIntoMovies(state, movies) {
+      state.movies.push(...movies);
     }
   },
   actions: {
@@ -24,13 +27,25 @@ export default {
         movies: [] //초기화
       });
       const res = await axios.get(
-        `http://www.omdbapi.com/?apikey=e2472a39&s=${state.title}`
+        `http://www.omdbapi.com/?apikey=e2472a39&s=${state.title}&page=1`
       );
       commit('updateState', {
-        movies: res.data.Search,
+        movies: res.data.Search
+      });
+      const pageLength = Math.ceil(res.data.totalResults / 10);
+      if (pageLength > 1) {
+        //더 가지고 올 아이템이 존재
+        for (let i = 2; i <= pageLength; i++) {
+          if (i > 4) break; //최대 40개까지의 아이템만 가져오기
+          const resMore = await axios.get(
+            `http://www.omdbapi.com/?apikey=e2472a39&s=${state.title}&page=${i}`
+          );
+          commit('pushIntoMovies', resMore.data.Search);
+        }
+      }
+      commit('updateState', {
         loading: false
       });
-      console.log(res);
     }
   }
 };
