d191e01c5fee0b4c661bc19100cd8ad0feb215b3
Sannim <inegg.apps@gmail.com>
2020-10-02 19:36:09 +0900

    Grid system in Vuetify

--- vue-04-movie-app/src/components/MovieList.vue
+++ vue-04-movie-app/src/components/MovieList.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>Movie</div>
+  <v-row>
+    <v-col
+      v-for="movie in movies"
+      :key="movie.imdbID"
+      cols="12"
+      lg="3"
+      md="3"
+      sm="2"
+    >
+      {{ movie.Title }}
+      {{ movie.Year }}
+      {{ movie.Poster }}
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    movies() {
+      return this.$store.state.movie.movies;
+    },
+  },
+};
 </script>
